a6383d458482afd0742c733d5e9b491c8c0779c7
walaa soudy <77520251+walaasoudy@users.noreply.github.com>
2025-06-10 13:20:31 +0200

    implement comment and like/unlike functionality for posts, enhance error handling, and add notifications for likes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -53,3 +53,96 @@ export const deletePost = asyncHandler(async (req, res) => {
 
   return res.status(200).json({ message: "Post deleted successfully" });
 });
+
+export const commentOnPost = asyncHandler(async (req, res) => {
+  const { text } = req.body;
+  const postId = req.params.id;
+  const userId = req.user._id;
+
+  if (!text) {
+    res.status(400);
+    throw new Error("Text field is required");
+  }
+
+  const post = await Post.findById(postId);
+  if (!post) {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+
+  const comment = { user: userId, text };
+  post.comments.push(comment);
+  await post.save();
+
+  await post.populate("comments.user", "-password");
+
+  return res.status(200).json(post);
+});
+
+export const likeUnlikePost = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const postId = req.params.id;
+
+  const post = await Post.findById(postId);
+  if (!post) {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+
+  const userLiked = post.likes.includes(userId);
+
+  if (userLiked) {
+
+    await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+    await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
+
+    const updatedLikes = post.likes.filter(
+      (id) => id.toString() !== userId.toString()
+    );
+
+    return res.status(200).json({
+      message: "Post unliked successfully",
+      likes: updatedLikes,
+    });
+  } else {
+
+    post.likes.push(userId);
+    await User.updateOne(
+      { _id: userId },
+      { $addToSet: { likedPosts: postId } }
+    );
+    await post.save();
+
+    if (userId.toString() !== post.user.toString()) {
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+      await notification.save();
+    }
+
+    return res.status(200).json({
+      message: "Post liked successfully",
+      likes: post.likes,
+    });
+  }
+});
+
+export const getAllPosts = asyncHandler(async (req, res) => {
+const posts = await Post.find()
+  .sort({ createdAt: -1 })
+  .populate({
+    path: "user",
+    select: "-password",
+  })
+  .populate({
+    path: "comments.user",
+    select: "-password",
+  });
+  if (posts.length === 0) {
+    res.status(200);
+    throw new Error("No posts found");
+  }
+  return res.status(200).json({ message: "data: ", posts });
+})
